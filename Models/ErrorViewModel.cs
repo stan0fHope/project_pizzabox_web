28b9344da110447f8306e937b61eb69378511ae0
Stanhope Nwosu <stanhope.nwosu@gmail.com>
2021-05-03 11:03:28 -0400

    Set up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace project_pizzabox_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
